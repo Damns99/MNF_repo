690ed9b6b8835ca98d9acfcd34e1748d48fe1760
damns99 <lucarelli.damiano.ld@gmail.com>
2022-06-15 23:32:34 +0200

    leapfrog e laxwendroff

--- MODULO_4/int_test.cpp
+++ MODULO_4/int_test.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 #include <math.h>
+#include <filesystem>
+namespace fs = std::filesystem;
 
 #include "bound_cond_vecs.h"
 #include "derivators.h"
@@ -11,7 +13,7 @@
 // equation: du/dt = df(u)/dx + d2g(u)/dx2
 
 std::vector<bound_cond_vecs::BoundCondVec<double>> f(std::vector<bound_cond_vecs::BoundCondVec<double>> u, double t, bound_cond_vecs::BoundCondVec<double> x) {
-	double v = 0.9;
+	double v = 0.99;
 	
 	std::vector<bound_cond_vecs::BoundCondVec<double>> fu;
 	for(int ii = 0; ii < u.size(); ii++) {
@@ -25,7 +27,7 @@ std::vector<bound_cond_vecs::BoundCondVec<double>> f(std::vector<bound_cond_vecs
 }
 
 std::vector<bound_cond_vecs::BoundCondVec<double>> g(std::vector<bound_cond_vecs::BoundCondVec<double>> u, double t, bound_cond_vecs::BoundCondVec<double> x) {
-	double nu = 0.;
+	double nu = 0.001;
 	
 	std::vector<bound_cond_vecs::BoundCondVec<double>> gu;
 	for(int ii = 0; ii < u.size(); ii++) {
@@ -39,20 +41,23 @@ std::vector<bound_cond_vecs::BoundCondVec<double>> g(std::vector<bound_cond_vecs
 }
 
 int main() {
-	double t0 = 0., dt = 0.01;
-	int nsteps = 500, nx = 100;
+	double t0 = 0., dt = 0.02;
+	int nsteps = 100, nx = 50;
 	double x0 = 0., dx = 1. / nx;
 	bound_cond_vecs::BoundCondVec<double> x = integrators::linspace(x0, x0 + nx * dx, nx);
 	std::vector<bound_cond_vecs::BoundCondVec<double>> u0;
 	bound_cond_vecs::BoundCondVec<double> u0_tmp(nx, x.getMode());
 	for(int ii = 0; ii < nx; ii++) {
-		u0_tmp[ii] = cos(2. * M_PI * x[ii]);
+		u0_tmp[ii] = cos(2. * M_PI * 4. * x[ii]) + 0.5 * sin(2. * M_PI * 2. * x[ii]);
 	}
 	u0.push_back(u0_tmp);
 	
 	auto u1 = integrators::FTCS(t0, dt, nsteps, x, u0, f, derivators::symm_derive, g, derivators::symm_derive_2);
-	auto u2 = integrators::Lax(t0, dt, nsteps, x, u0, f, derivators::symm_derive, g, derivators::symm_derive_2);
+	auto u2 = integrators::Lax(t0, dt, nsteps, x, u0, f, derivators::symm_derive, g, derivators::symm_derive_2);	
+	auto u3 = integrators::LeapFrog(t0, dt, nsteps, x, u0, f, derivators::symm_derive, g, derivators::symm_derive_2);
+	auto u4 = integrators::LaxWendroff(t0, dt, nsteps, x, u0, f, derivators::fwd_derive, g, derivators::symm_derive_2);
 	
+	fs::current_path(fs::current_path() / "measures");
 	double maxu = 2.;
 	waveplots::plot(u1, t0, dt, nsteps, x0, dx, nx, "first_FTCS_test_SURF", SURF_PLOT, maxu);
 	waveplots::plot(u1, t0, dt, nsteps, x0, dx, nx, "first_FTCS_test_CONT", CONT_PLOT, maxu);
@@ -60,4 +65,10 @@ int main() {
 	waveplots::plot(u2, t0, dt, nsteps, x0, dx, nx, "first_Lax_test_SURF", SURF_PLOT, maxu);
 	waveplots::plot(u2, t0, dt, nsteps, x0, dx, nx, "first_Lax_test_CONT", CONT_PLOT, maxu);
 	waveplots::plot(u2, t0, dt, nsteps, x0, dx, nx, "first_Lax_test_COLZ", COLZ_PLOT, maxu);
+	waveplots::plot(u3, t0, dt, nsteps, x0, dx, nx, "first_LeapFrog_test_SURF", SURF_PLOT, maxu);
+	waveplots::plot(u3, t0, dt, nsteps, x0, dx, nx, "first_LeapFrog_CONT", CONT_PLOT, maxu);
+	waveplots::plot(u3, t0, dt, nsteps, x0, dx, nx, "first_LeapFrog_COLZ", COLZ_PLOT, maxu);
+	waveplots::plot(u4, t0, dt, nsteps, x0, dx, nx, "first_LaxWendroff_test_SURF", SURF_PLOT, maxu);
+	waveplots::plot(u4, t0, dt, nsteps, x0, dx, nx, "first_LaxWendroff_test_CONT", CONT_PLOT, maxu);
+	waveplots::plot(u4, t0, dt, nsteps, x0, dx, nx, "first_LaxWendroff_test_COLZ", COLZ_PLOT, maxu);
 }
--- MODULO_4/integrators.h
+++ MODULO_4/integrators.h
@@ -48,7 +48,7 @@ namespace integrators {
 					DblBcv d1fu = d1(fu[jj], dx), d2gu = d2(gu[jj], dx), tmp_u(x.len(), x.getMode());
 					for(int kk = 0; kk < x.len(); kk++) tmp_u[kk] = u[ii- 1][jj][kk] + dt * (d1fu[kk] + d2gu[kk]);
 					new_u.push_back(tmp_u);
-				}					
+				}
 				u.push_back(new_u);
 				t.push_back(t0 + ii * dt);
 			}
@@ -79,8 +79,126 @@ namespace integrators {
 					DblBcv d1fu = d1(fu[jj], dx), d2gu = d2(gu[jj], dx), tmp_u(x.len(), x.getMode());
 					for(int kk = 0; kk < x.len(); kk++) tmp_u[kk] = (u[ii- 1][jj][kk - 1] + u[ii- 1][jj][kk + 1]) / 2 + dt * (d1fu[kk] + d2gu[kk]);
 					new_u.push_back(tmp_u);
-				}					
+				}
 				u.push_back(new_u);
+				t.push_back(t0 + ii * dt);
+			}
+			
+			return u;
+		}
+		
+		std::vector<std::vector<DblBcv>> LeapFrog_function(
+			double t0, double dt, double nsteps, const DblBcv & x, const std::vector<DblBcv> & u0, 
+			std::vector<DblBcv> (*f)(std::vector<DblBcv>, double, DblBcv), derivators::Derivator d1, 
+			std::vector<DblBcv> (*g)(std::vector<DblBcv>, double, DblBcv), derivators::Derivator d2) 
+		{
+			double dx = x[1] - x[0];
+			int ndim = u0.size();
+			std::vector<std::vector<DblBcv>> u;
+			u.reserve(nsteps + 1);
+			u.push_back(u0);
+			std::vector<double> t;
+			t.reserve(nsteps + 1);
+			t.push_back(t0);
+			auto u1 = FTCS_function(t0, dt, 1, x, u0, f, d1, g, d2);
+			u.push_back(u1[1]);
+			t.push_back(t0 + dt);
+			
+			for(int ii = 2; ii <= nsteps; ii++) {
+				std::vector<DblBcv> new_u;
+				new_u.reserve(ndim);
+				std::vector<DblBcv> fu = f(u[ii - 1], t0 + (ii - 1) * dt, x);
+				std::vector<DblBcv> gu = g(u[ii - 1], t0 + (ii - 1) * dt, x);
+				for(int jj = 0; jj < ndim; jj++) {
+					DblBcv d1fu = d1(fu[jj], dx), d2gu = d2(gu[jj], dx), tmp_u(x.len(), x.getMode());
+					for(int kk = 0; kk < x.len(); kk++) tmp_u[kk] = u[ii- 2][jj][kk] + 2. * dt * (d1fu[kk] + d2gu[kk]);
+					new_u.push_back(tmp_u);
+				}
+				u.push_back(new_u);
+				t.push_back(t0 + ii * dt);
+			}
+			
+			return u;
+		}
+		
+		std::vector<std::vector<DblBcv>> LaxWendroff_function(
+			double t0, double dt, double nsteps, const DblBcv & x, const std::vector<DblBcv> & u0, 
+			std::vector<DblBcv> (*f)(std::vector<DblBcv>, double, DblBcv), derivators::Derivator d1, 
+			std::vector<DblBcv> (*g)(std::vector<DblBcv>, double, DblBcv), derivators::Derivator d2) 
+		{
+			double dx = x[1] - x[0];
+			int ndim = u0.size();
+			std::vector<std::vector<DblBcv>> u;
+			u.reserve(nsteps + 1);
+			u.push_back(u0);
+			std::vector<double> t;
+			t.reserve(nsteps + 1);
+			t.push_back(t0);
+			DblBcv x2 = x;
+			for(int ii = 0; ii < x2.len(); ii++) x2[ii] += 0.5 * dx;
+			
+			for(int ii = 1; ii <= nsteps; ii++) {
+				std::vector<DblBcv> new_u, new_u2;
+				new_u.reserve(ndim);
+				new_u2.reserve(ndim);
+				std::vector<DblBcv> fu = f(u[ii - 1], t0 + (ii - 1) * dt, x);
+				std::vector<DblBcv> gu = g(u[ii - 1], t0 + (ii - 1) * dt, x);
+				for(int jj = 0; jj < ndim; jj++) {
+					DblBcv d1fu = d1(fu[jj], dx), d2gu = d2(gu[jj], dx), tmp_u(x.len(), x.getMode());
+					for(int kk = 0; kk < x.len(); kk++) tmp_u[kk] = (u[ii- 1][jj][kk] + u[ii- 1][jj][kk + 1]) / 2 + dt / 2 * (d1fu[kk] + d2gu[kk]);
+					new_u.push_back(tmp_u);
+				}
+				std::vector<DblBcv> fu2 = f(new_u, t0 + (ii - 0.5) * dt, x2);
+				std::vector<DblBcv> gu2 = g(new_u, t0 + (ii - 0.5) * dt, x2);
+				for(int jj = 0; jj < ndim; jj++) {
+					DblBcv d1fu = d1(fu2[jj], dx), d2gu = d2(gu2[jj], dx), tmp_u(x.len(), x.getMode());
+					for(int kk = 0; kk < x.len(); kk++) tmp_u[kk] = u[ii- 1][jj][kk] + dt * (d1fu[kk - 1] + d2gu[kk - 1]);
+					new_u2.push_back(tmp_u);
+				}
+				
+				u.push_back(new_u2);
+				t.push_back(t0 + ii * dt);
+			}
+			
+			return u;
+		}
+		
+		std::vector<std::vector<DblBcv>> LaxWendroff2_function(
+			double t0, double dt, double nsteps, const DblBcv & x, const std::vector<DblBcv> & u0, 
+			std::vector<DblBcv> (*f)(std::vector<DblBcv>, double, DblBcv), derivators::Derivator d1, 
+			std::vector<DblBcv> (*g)(std::vector<DblBcv>, double, DblBcv), derivators::Derivator d2) 
+		{
+			double dx = x[1] - x[0];
+			int ndim = u0.size();
+			std::vector<std::vector<DblBcv>> u;
+			u.reserve(nsteps + 1);
+			u.push_back(u0);
+			std::vector<double> t;
+			t.reserve(nsteps + 1);
+			t.push_back(t0);
+			DblBcv x2 = x;
+			for(int ii = 0; ii < x2.len(); ii++) x2[ii] += 0.5 * dx;
+			
+			for(int ii = 1; ii <= nsteps; ii++) {
+				std::vector<DblBcv> new_u, new_u2;
+				new_u.reserve(ndim);
+				new_u2.reserve(ndim);
+				std::vector<DblBcv> fu = f(u[ii - 1], t0 + (ii - 1) * dt, x);
+				std::vector<DblBcv> gu = g(u[ii - 1], t0 + (ii - 1) * dt, x);
+				for(int jj = 0; jj < ndim; jj++) {
+					DblBcv tmp_u(x.len(), x.getMode());
+					for(int kk = 0; kk < x.len(); kk++) tmp_u[kk] = (u[ii- 1][jj][kk] + u[ii- 1][jj][kk + 1]) / 2 + dt / 2 * (fu[jj][kk + 1] - fu[jj][kk]) / dx;
+					new_u.push_back(tmp_u);
+				}
+				std::vector<DblBcv> fu2 = f(new_u, t0 + (ii - 0.5) * dt, x2);
+				std::vector<DblBcv> gu2 = g(new_u, t0 + (ii - 0.5) * dt, x2);
+				for(int jj = 0; jj < ndim; jj++) {
+					DblBcv tmp_u(x.len(), x.getMode());
+					for(int kk = 0; kk < x.len(); kk++) tmp_u[kk] = u[ii- 1][jj][kk] + dt * (fu2[jj][kk] - fu2[jj][kk - 1]) / dx;
+					new_u2.push_back(tmp_u);
+				}
+				
+				u.push_back(new_u2);
 				t.push_back(t0 + ii * dt);
 			}
 			
@@ -91,6 +209,8 @@ namespace integrators {
 	
 	Integrator FTCS = &(utilities::FTCS_function);
 	Integrator Lax = &(utilities::Lax_function);
+	Integrator LeapFrog = &(utilities::LeapFrog_function);
+	Integrator LaxWendroff = &(utilities::LaxWendroff_function);
 	
 }
 
