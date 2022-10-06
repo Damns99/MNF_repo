17260a39ad1007a0b9dd4d9e3ae2973f0e902b90
damns99 <lucarelli.damiano.ld@gmail.com>
2022-10-06 17:39:35 +0200

    start final & new index

---
+++ MODULO_1/square_ising_final/lattice.h
@@ -0,0 +1,57 @@
+#ifndef LATTICE_H
+#define LATTICE_H
+
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <cassert>
+#include <fstream>
+#include <sstream>
+
+#include "ran2.h"
+
+constexpr int MAX_LENGTH = 65536; // = 2^16
+
+constexpr int JACC = 1;
+
+constexpr int HOT = 1;
+constexpr int COLD = 2;
+constexpr int FROM_FILE = 3;
+
+inline int index1d(int i, int L) {
+    return ((i %= L) < 0) ? i + L : i;
+}
+inline int index2d(int i, int j, int L) {
+	return L * index1d(i, L) + index1d(j, L);
+}
+
+inline int spin[MAX_LENGTH];
+inline int length;
+inline double beta;
+inline double extrafield;
+inline double magnetization;
+inline double energy;
+inline ran2::RandomGenerator gen;
+		
+void latticeFromFile(std::string input_file);
+
+void createLattice(int len, double inv_temp, double extf, long seed, int init_mode, const std::string init_file);
+
+void spinInit(int init_mode, const std::string init_file);
+
+void calculateEnergyMagnetizaton();
+
+void spinToFile(const std::string output_file);
+
+void spinFromFile(const std::string init_file);
+
+void save(const std::string output_file);
+
+void load(const std::string input_file);
+
+void snapshot(const std::string out_image);
+
+int updateMetropolis();
+int updateMetropolis2();
+
+#endif
