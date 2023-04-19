491a1ad6b8de3f8f38dcf31deaca86ab135ce789
damns99 <lucarelli.damiano.ld@gmail.com>
2023-04-20 00:14:55 +0200

    start to work

--- MODULO_8/integrate_LIF.cpp
+++ MODULO_8/integrate_LIF.cpp
@@ -30,7 +30,7 @@ std::vector<double> logspace(double e0, double e1, double n = 100) {
 
 // Forward Euler method
 // dV(n)/dt = (V(n+1) - V(n)) / h
-std::vector<double> fwdEuler1(double V0, double h, int N, std::vector<double> I, double params[5]) {
+std::vector<double> fwdEuler1(double V0, double h, int N, std::vector<double>& I, double params[5]) {
 	double Cm = params[0], g = params[1], Vrest = params[2], Vth = params[3], Vreset = params[4];
 	std::vector<double> V(N+1);
 	V[0] = V0;
@@ -40,7 +40,7 @@ std::vector<double> fwdEuler1(double V0, double h, int N, std::vector<double> I,
 	return V;
 }
 
-std::pair<std::vector<double>,std::vector<double>> fwdEuler2(double V0, double t0, double h, int N, std::vector<double> I, double params[5]) {
+std::pair<std::vector<double>,std::vector<double>> fwdEuler2(double V0, double t0, double h, int N, std::vector<double>& I, double params[5]) {
 	double Cm = params[0], g = params[1], Vrest = params[2], Vth = params[3], Vreset = params[4];
 	std::vector<double> V(N+1), t(N+1);
 	V[0] = V0; t[0] = t0;
@@ -51,9 +51,62 @@ std::pair<std::vector<double>,std::vector<double>> fwdEuler2(double V0, double t
 	return std::make_pair(V,t);
 }
 
+std::vector<double> fwdEuler3(double V0, double h, int N, std::vector<double>& I, double params[5]) {
+	double Cm = params[0], g = params[1], Vrest = params[2], Vth = params[3], Vreset = params[4];
+	std::vector<double> V;
+	V.push_back(V0);
+	for(int n = 0; n < N; n++) {
+		V.push_back(V[n] * (1 - g/Cm*h) + h/Cm * I[n]);
+	}
+	return V;
+}
+
+void fwdEuler4(double V0, double h, int N, double I[], double params[5], double* V) {
+	double Cm = params[0], g = params[1], Vrest = params[2], Vth = params[3], Vreset = params[4];
+	V[0] = V0;
+	for(int n = 0; n < N; n++) {
+		V[n+1] = V[n] * (1 - g/Cm*h) + h/Cm * I[n];
+	}
+}
+
+void fwdEuler5(double V0, double t0, double h, int N, double I[], double params[5], double* V, double* t) {
+	double Cm = params[0], g = params[1], Vrest = params[2], Vth = params[3], Vreset = params[4];
+	V[0] = V0; t[0] = t0;
+	for(int n = 0; n < N; n++) {
+		V[n+1] = V[n] * (1 - g/Cm*h) + h/Cm * I[n];
+		t[n+1] = t0 + (n+1) * h;
+	}
+}
+
 int main() {
+	double params[5] = {
+		1., // Cm [ms/ohm]
+		1., // g [1/ohm]
+		-70., // Vrest [mV]
+		40., // Vth [mV]
+		-65.  // Vreset [mV]
+	};
+	double h = 0.1; // ms
+	int N = 1000;
+	std::vector<double> I = square_wave(); // da definireeeeeeeeeeeeeeee // mA
+	double t0 = 0.; // ms
+	double V0 = -70.; // mV
+	
+	{
 	std::chrono::time_point<std::chrono::steady_clock> start, end;
 	start = std::chrono::steady_clock::now();
+	std::vector<double> V = fwdEuler1(V0, h, N, I, params);
+	std::vector<double> t = linspace(t0, t0 + N*h, N);
 	end = std::chrono::steady_clock::now();
-	std::cout << "updated + measured in: " << (end - start).count() / 1000000. << " s" << std::endl;
+	std::cout << "fwdEuler1: " << (end - start).count() / 1000000. << " s" << std::endl;
+	}
+	
+	{
+	std::chrono::time_point<std::chrono::steady_clock> start, end;
+	start = std::chrono::steady_clock::now();
+	std::pair<std::vector<double>,std::vector<double>> res = fwdEuler2(V0, t0, h, N, I, params);
+	std::vector<double> V = res.first, t = res.second;
+	end = std::chrono::steady_clock::now();
+	std::cout << "fwdEuler2: " << (end - start).count() / 1000000. << " s" << std::endl;
+	}
 }
